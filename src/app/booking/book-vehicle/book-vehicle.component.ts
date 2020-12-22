5153e168ea8ad5622ea886317d3fd0bd0a5ac642
faisal <faisal25march99@gmail.com>
2020-12-22 10:44:54 +0530

    Done with checkpoint 2

---
+++ src/app/booking/book-vehicle/book-vehicle.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import Stepper from 'bs-stepper';
+
+@Component({
+  selector: 'app-book-vehicle',
+  templateUrl: './book-vehicle.component.html',
+  styleUrls: ['./book-vehicle.component.scss']
+})
+export class BookVehicleComponent implements OnInit {
+  private stepper: Stepper;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.stepper = new Stepper(document.querySelector('#stepper1'), {
+      linear: false,
+      animation: true
+    })
+  }
+  prev() {
+    this.stepper.previous();
+  }
+
+  next() {
+    this.stepper.next();
+  }
+
+  onSubmit() {
+    return false;
+  }
+}
